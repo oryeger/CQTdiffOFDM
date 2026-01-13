1455b6323560559219797de3dcdf980ddb62ec2b
Ory Eger <egerory@gmail.com>
2026-01-13 17:20:48 +0200

    Before adding EVM

--- CQTdiffMatlab/plot_three.m
+++ CQTdiffMatlab/plot_three.m
@@ -2,13 +2,13 @@
 main_dir = 'declipping05_01_2026';
 subplot(3,1,1);
 lim_val = 0.25;
-plot_wav_channels('C:\Projects\CQTdiffOFDM\notebooks\experiments\cqt\declipping06_01_2026\original\1.wav')
+plot_wav_channels('C:\Projects\CQTdiffOFDM\notebooks\experiments\cqt\declipping13_01_2026\original\1.wav')
 title('Orig')
 ylim([-lim_val lim_val]);
-plot_wav_channels('C:\Projects\CQTdiffOFDM\notebooks\experiments\cqt\declipping06_01_2026\clipped_3\1.wav')
+plot_wav_channels('C:\Projects\CQTdiffOFDM\notebooks\experiments\cqt\declipping13_01_2026\clipped_3\1.wav')
 title('Clipped')
 ylim([-lim_val lim_val]);
-plot_wav_channels('C:\Projects\CQTdiffOFDM\notebooks\experiments\cqt\declipping06_01_2026\declipped_3\1.wav')
+plot_wav_channels('C:\Projects\CQTdiffOFDM\notebooks\experiments\cqt\declipping13_01_2026\declipped_3\1.wav')
 title('Declipped')
 ylim([-lim_val lim_val]);
 
